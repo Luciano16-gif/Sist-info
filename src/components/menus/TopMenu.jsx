7ba9dcc109c1d1b90b25b6b5b25f3dc5091c1239
Luciano 16-gif <chanominardo10@gmail.com>
2025-03-03 17:05:44 -0400

    text is now centered, and the height of the top-menu is better handeled

--- src/components/menus/TopMenu.jsx
+++ src/components/menus/TopMenu.jsx
@@ -31,29 +31,30 @@ const TopMenu = () => {
         right-0
         bg-opacity-85
         space-x-12
+        min-h-16
       "
       style={{zIndex: 9999}}
     >
-      <div>
-        <img class="max-h-12 min-w-20 min-h-7 space-x-5 px-2"
+      <div className="flex items-center justify-center">
+        <img className="max-h-12 min-w-20 min-h-7 px-2"
         src="/src/assets/images/Logo_Avilaventuras.webp"
         alt="Avilaventuras"
         />
       </div>
-      <p class="uppercase font-ysabeau text-gray-400">Visitante</p>
-      <ul className="flex flex-row uppercase font-ysabeau underline sm:text-sm sm:space-x-12">
+      <p className="uppercase font-ysabeau text-gray-400 flex items-center justify-center">Visitante</p>
+      <ul className="flex flex-row uppercase font-ysabeau underline sm:text-sm sm:space-x-12 justify-center">
             {menuItems.map((item) => (
-            <li key={item.href} className="hover:scale-110 transform transition-all duration-300">
-                <Link to={item.href}>{item.label}</Link>
+            <li key={item.href} className="hover:scale-110 transform transition-all duration-300 flex items-center">
+                <Link to={item.href} className="text-center">{item.label}</Link>
             </li>
             ))}
       </ul>
-      <div className="">
-        <ul className="flex flex-row uppercase font-ysabeau sm:text-sm sm:space-x-12">
+      <div className="flex justify-center">
+        <ul className="flex flex-row uppercase font-ysabeau sm:text-sm sm:space-x-2">
           {sesionItems.map((item) => (
-              <li key={item.href} className="bg-gray-700 box-border p-2 border-gray-200 border rounded-full
-              hover:scale-110 transform transition-all duration-300">
-                  <Link to={item.href}>{item.label}</Link>
+              <li key={item.href} className="bg-gray-800 box-border px-4 py-2 border-gray-200 border rounded-full
+              hover:scale-110 transform transition-all duration-300 flex items-center justify-center">
+                  <Link to={item.href} className="text-center whitespace-nowrap">{item.label}</Link>
               </li>))}
         </ul>
       </div>
