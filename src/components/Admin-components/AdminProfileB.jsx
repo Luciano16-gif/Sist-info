5d3c9add900b9bac0af412e64d8c8075cdba3842
Eacosta <esteacosta@Estebans-MacBook-Pro.local>
2025-03-11 20:37:47 -0400

    Creación del archivo AdminProfileB.jsx en Admin-components

---
+++ src/components/Admin-components/AdminProfileB.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react'; // Add useEffect
+
+const AdminProfileB = () => {
+    return (
+      <div></div>
+    );
+  };
+  
+  export default AdminProfileB;
--- src/components/layout/LayoutAdmin.jsx
+++ src/components/layout/LayoutAdmin.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from "../menus/Sidebar";
+import AdminProfileB from "../Admin-components/AdminProfileB"
 
 const LayoutAdmin = () => {
     return (
       <div className="h-screen bg-[#0F180B] bg-scroll">
+        <AdminProfileB />
         <Sidebar />
         <Outlet />
       </div>
