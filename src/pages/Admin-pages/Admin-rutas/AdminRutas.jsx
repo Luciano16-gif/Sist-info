e4c48d601b33debc22420ea32b46fb5d7b914abc
RicardoEPaez <ricardo.paez@correo.unimet.edu.ve>
2025-03-14 11:21:58 -0400

    AdminRutas

--- src/pages/Admin-pages/Admin-rutas/AdminRutas.jsx
+++ src/pages/Admin-pages/Admin-rutas/AdminRutas.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'; // Add useEffect
+import React, { useEffect } from 'react';
 import RelevantInfoS from "../../../components/Admin-components/admin-buttons/InfoSection"
 
 const AdminRutas = () => {
@@ -14,7 +14,7 @@ const AdminRutas = () => {
   const ExcursionesEliminadas = 20;
 
   return (
-    <div className="absolute inset-0 mx-32 my-8 flex flex-col justify-start items-start px-8 md:px-16 space-y-4 z-10">
+    <div className="absolute inset-0 mx-8 md:mx-32 my-8 flex flex-col justify-start items-start px-4 md:px-16 space-y-4 z-10">
       <h1 className=" text-white text-4xl md:text-5xl font-bold">
         Rutas y Excursiones
       </h1>
@@ -26,19 +26,19 @@ const AdminRutas = () => {
       <h1 className=" text-white text-xl md:text-3xl font-bold">
         Información relevante
       </h1>
-      <div className="flex justify-start space-x-10 overflow-x">
-        <RelevantInfoS number = {ExcursionesDisponibles} description ="Excursiones Disponibles" />
-        <RelevantInfoS number = {ExcursionesNInscritas} description ="Excursiones sin Inscripciones" />
-        <RelevantInfoS number = {ExcursionesNGuias} description ="Excursiones sin Guías Inscritos" />
-        <RelevantInfoS number = {ExcursionesRealizadas} description ="Excursiones realizadas en total" />
-        <RelevantInfoS number = {PromedioPart} description ="Promedio de participantes por mes" />
-        <RelevantInfoS number = {RutasAnadidas} description ="Rutas añadidas en el último mes" />
-        <RelevantInfoS number = {HorasExcursiones} description ="Horas en Excursiones" />
-        <RelevantInfoS number = {ExcursionesEliminadas} description ="Excursiones Eliminadas" />
+      <div className="flex flex-nowrap overflow-x-auto space-x-4"> 
+        <RelevantInfoS number={ExcursionesDisponibles} description="Excursiones Disponibles"  />
+        <RelevantInfoS number={ExcursionesNInscritas} description="Excursiones sin Inscripciones"  />
+        <RelevantInfoS number={ExcursionesNGuias} description="Excursiones sin Guías Inscritos"  />
+        <RelevantInfoS number={ExcursionesRealizadas} description="Excursiones realizadas en total"  />
+        <RelevantInfoS number={PromedioPart} description="Promedio de participantes por mes"  />
+        <RelevantInfoS number={RutasAnadidas} description="Rutas añadidas en el último mes"  />
+        <RelevantInfoS number={HorasExcursiones} description="Horas en Excursiones"  />
+        <RelevantInfoS number={ExcursionesEliminadas} description="Excursiones Eliminadas"  />
       </div>
       <hr className="border-1 border-white-600 sm:w-10 md:w-96" />
     </div>
   );
 };
-  
-  export default AdminRutas;
+
+export default AdminRutas;
