6c28d2e7f13c120da12afccc148f6332b011d8bb
Sofia Machado <sofia.machado@correo.unimet.edu.ve>
2025-03-16 14:04:50 -0400

    Hover de cartas de tips

--- src/components/Admin-components/admin-buttons/TipCard.jsx
+++ src/components/Admin-components/admin-buttons/TipCard.jsx
@@ -1,9 +1,16 @@
 // TipCard.jsx
-import React from 'react';
+import React, { useState } from 'react';
 
 const TipCard = ({ imageSrc, title, description }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-        <div className="bg-[#556052] w-[311px] h-[480px] text-white p-6 rounded-xl flex items-center justify-center">
+        <div 
+            className={`bg-[#556052] w-[311px] h-[480px] text-white p-6 rounded-xl flex items-center justify-center relative overflow-hidden ${isHovered ? 'bg-black bg-opacity-70' : ''}`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            {/* Contenido de la carta */}
             <div className="bg-[#556052] w-[271.261px] h-[444.167px] text-white rounded-xl flex flex-col items-center border-2 border-solid">
                 {/* Parte superior de la carta */}
                 <div className="flex flex-col p-12 items-center justify-center">
@@ -15,8 +22,26 @@ const TipCard = ({ imageSrc, title, description }) => {
 
                 {/* Parte inferior de la carta */}
                 <h2 className="text-[26px] font-bold text-center mt-2 leading-7">{title}</h2>
-                <p className="text-center text-[15px] p-5 leading-5">{description}</p>
+                <p className="text-center text-[15px] p-4 leading-5">{description}</p>
             </div>
+
+            {/* Botones de Editar y Eliminar */}
+            {isHovered && (
+                <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-70">
+                    <button 
+                        className="bg-gray-400 hover:bg-[#EFDE5F] text-white font-bold py-2 px-4 rounded-full mb-4"
+                        onClick={() => console.log('Edit')}
+                    >
+                        Editar
+                    </button>
+                    <button 
+                        className="bg-gray-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded-full"
+                        onClick={() => console.log('Delete')}
+                    >
+                        Eliminar
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
