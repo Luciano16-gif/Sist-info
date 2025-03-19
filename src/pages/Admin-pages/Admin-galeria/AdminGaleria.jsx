6e2f63c1bb68093616994f1c9991e4cd6ac80052
Santiago Zabala <szabala@correo.unimet.edu.ve>
2025-03-18 22:01:38 -0400

    Se arreglaron algunas páginas responsive

--- src/pages/Admin-pages/Admin-galeria/AdminGaleria.jsx
+++ src/pages/Admin-pages/Admin-galeria/AdminGaleria.jsx
@@ -115,19 +115,19 @@ const AdminGaleria = () => {
     }
 
     return (
-        <div className={`overflow-x-scroll my-8 flex flex-col justify-start items-start px-8 md:px-16 ${adminBaseStyles}`}>
-            <h1 className=" text-white text-4xl md:text-5xl font-bold">
+        <div className={`inset-0 mx-4 md:mx-8 lg:mx-32 my-8 flex flex-col justify-start items-start px-4 md:px-8 lg:px-16 ${adminBaseStyles}`}>
+            <h1 className="text-white text-3xl md:text-4xl lg:text-5xl font-bold">
                 Galería
             </h1>
-            <h1 className=" text-white text-lg md:text-lg">
+            <h1 className="text-white text-base md:text-lg">
                 Representa el espacio donde nuestros usuarios y guías podrán echar un vistazo
                 de cómo son las actividades que ofrecemos.
             </h1>
-            <hr className="border-1 border-white-600 sm:w-10 md:w-96" />
-            <h1 className=" text-white text-xl md:text-3xl font-bold">
+            <hr className="border-1 border-white-600 w-full md:w-96" />
+            <h1 className="text-white text-2xl md:text-3xl font-bold">
                 Información relevante
             </h1>
-            <div className="flex justify-start space-x-10">
+            <div className="flex flex-wrap justify-start gap-4 md:gap-6 lg:gap-10 my-4">
                 <RelevantInfoS number={publishedImages} description="Imágenes publicadas" />
                 <RelevantInfoS number={reportedImages} description="Imágenes reportadas" />
                 <RelevantInfoS number={participatingUsers} description="Usuarios participantes" />
@@ -135,8 +135,8 @@ const AdminGaleria = () => {
                 <RelevantInfoS number={usedHashtags} description="Hashtags usados" />
             </div>
             <div className="flex flex-col">
-                <h2 className="text-4xl font-bold text-white mr-4">Nuestra Galería</h2>
-                <h2 className="text-sm text-white mr-4">Selecciona una imagen para obtener
+                <h2 className="text-xl md:text-2xl lg:text-4xl font-bold text-white mr-4">Nuestra Galería</h2>
+                <h2 className="text-xs md:text-sm text-white mr-4">Selecciona una imagen para obtener
                     los detalles de la misma o eliminarla de nuestra galería
                 </h2>
             </div>
